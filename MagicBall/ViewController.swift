c4839818cbb49cf10b5480b0bc46f468c8e2a894
Alfonso Cano Vargas <yveltal02341@outlook.com>
2023-01-18 22:58:21 -0800

    Initial Commit

---
+++ MagicBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicBall
+//
+//  Created by Alfonso on 28/10/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
